119a05b2cac76f4321a469ba71fdbbbeca30aba1
Evgeniy Orlov <sonic050288@gmail.com>
2019-07-09 18:59:14 +0300

    Write SQL statements for INSERT

--- app.py
+++ app.py
@@ -1,47 +1,85 @@
 import sqlite3
 
 conn = sqlite3.connect("crossfit-diary.db")
+cursor = conn.cursor()
 
+cursor.execute('''
+	CREATE TABLE IF NOT EXISTS days (
+	ROWID INTEGER NOT NULL,
+	date TEXT NOT NULL,
+	skill_id INT NOT NULL,
+	wod_id INT NOT NULL,
+	PRIMARY KEY(ROWID),
+	FOREIGN KEY (skill_id) REFERENCES skills(ROWID),
+	FOREIGN KEY (wod_id) REFERENCES wods(ROWID)
+	);'''
+)
 
+cursor.execute('''CREATE TABLE IF NOT EXISTS skills (
+	ROWID INTEGER NOT NULL,
+	type TEXT NOT NULL,
+	timecap TEXT NOT NULL,
+	PRIMARY KEY(ROWID),
+	FOREIGN KEY (type) REFERENCES type_complex(ROWID)
+	);'''
+)
 
-# sql = '''
-# 	CREATE TABLE IF NOT EXISTS days (
-# 	date TEXT NOT NULL,
-# 	skill_id INT NOT NULL,
-# 	wod_id INT NOT NULL,
-# 	FOREIGN KEY (skill_id) REFERENCES skills(ROWID),
-# 	FOREIGN KEY (wod_id) REFERENCES wods(ROWID)
-# 	);
+cursor.execute('''
+	CREATE TABLE IF NOT EXISTS wods (
+	ROWID INTEGER NOT NULL,
+	type TEXT NOT NULL,
+	timecap TEXT NOT NULL,
+	PRIMARY KEY(ROWID),
+	FOREIGN KEY (type) REFERENCES type_complex(ROWID)
+	);'''
+)
 
-# 	CREATE TABLE IF NOT EXISTS skills (
-# 	type TEXT NOT NULL,
-# 	timecap TEXT NOT NULL,
-# 	FOREIGN KEY (type) REFERENCES type_complex(ROWID)
-# 	);
+cursor.execute('''
+	CREATE TABLE IF NOT EXISTS type_complex (
+	ROWID INTEGER NOT NULL,
+	name TEXT NOT NULL,
+	PRIMARY KEY(ROWID)
+	);'''
+)
 
-# 	CREATE TABLE IF NOT EXISTS wods (
-# 	type TEXT NOT NULL,
-# 	timecap TEXT NOT NULL,
-# 	FOREIGN KEY (type) REFERENCES type_complex(ROWID)
-# 	);
+cursor.execute('''
+	CREATE TABLE IF NOT EXISTS rounds (
+	ROWID INTEGER NOT NULL,
+	skill_id INT NOT NULL,
+	wod_id INT NOT NULL,
+	PRIMARY KEY(ROWID),
+	FOREIGN KEY (skill_id) REFERENCES skills(ROWID),
+	FOREIGN KEY (wod_id) REFERENCES wods(ROWID)
+	);'''
+)
 
-# 	CREATE TABLE IF NOT EXISTS type_complex (
-# 	name TEXT NOT NULL,
-# 	);
+cursor.execute('''
+	CREATE TABLE IF NOT EXISTS exercise (
+	ROWID INTEGER NOT NULL,
+	round_id INT NOT NULL,
+	repeats INT NOT NULL,
+	weight INT NOT NULL,
+	name TEXT NOT NULL,
+	PRIMARY KEY(ROWID),
+	FOREIGN KEY (round_id) REFERENCES rounds(ROWID)
+	);'''
+)
 
-# 	CREATE TABLE IF NOT EXISTS rounds (
-# 	skill_id INT NOT NULL,
-# 	wod_id INT NOT NULL,
-# 	FOREIGN KEY (skill_id) REFERENCES skills(ROWID),
-# 	FOREIGN KEY (wod_id) REFERENCES wods(ROWID)
-# 	);
+# date = input("Дата: ")
+skill_type = input("Тип навыка ")
+skill_timecap = input("Время навыка ")
 
-# 	CREATE TABLE IF NOT EXISTS exercise (
-# 	round_id INT NOT NULL,
-# 	repeats INT NOT NULL,
-# 	weight INT NOT NULL,
-# 	name TEXT NOT NULL,
-# 	FOREIGN KEY (round_id) REFERENCES rounds(ROWID),
-# 	);
+cursor.execute('''
+	INSERT INTO skills ('timecap', type)
+	SELECT ? as timecap, ROWID FROM type_complex 
+	WHERE name = ?
+''', (skill_timecap, skill_type))
 
-# '''
+conn.commit()
+
+# skill_round_time = input("Время подхода навыка")
+# skill_exercise = input("Название упражнения")
+# skill_rounds = []
+# for count in range(5):
+# 	skill_rounds_masse = int(input("Масса подхода навыка"))
+# 	skill_rounds.append((date, skill_type,))
