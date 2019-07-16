707044a6c7c08781d04816c3563697af08d6fede
Evgeniy Orlov <sonic050288@gmail.com>
2019-07-16 18:50:10 +0300

    Add function for DB

--- app.py
+++ app.py
@@ -1,79 +1,48 @@
 import sqlite3
+import db
 
+# Создание Базы данных
 conn = sqlite3.connect("crossfit-diary.db")
-cursor = conn.cursor()
+
+db.create_db(conn)
+
+#Заполняю БД
+date = "2019-07-16"
+skill_type = "emom"
+skill_timecap = '12'
+
+db.input_skills(conn, skill_timecap, skill_type)
+
+wod_type = "for_time"
+wod_timecap = '15'
+
+
 
 cursor.execute('''
-	CREATE TABLE IF NOT EXISTS days (
-	ROWID INTEGER NOT NULL,
-	date TEXT NOT NULL,
-	skill_id INT NOT NULL,
-	wod_id INT NOT NULL,
-	PRIMARY KEY(ROWID),
-	FOREIGN KEY (skill_id) REFERENCES skills(ROWID),
-	FOREIGN KEY (wod_id) REFERENCES wods(ROWID)
-	);'''
-)
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS skills (
-	ROWID INTEGER NOT NULL,
-	type TEXT NOT NULL,
-	timecap TEXT NOT NULL,
-	PRIMARY KEY(ROWID),
-	FOREIGN KEY (type) REFERENCES type_complex(ROWID)
-	);'''
-)
-
-cursor.execute('''
-	CREATE TABLE IF NOT EXISTS wods (
-	ROWID INTEGER NOT NULL,
-	type TEXT NOT NULL,
-	timecap TEXT NOT NULL,
-	PRIMARY KEY(ROWID),
-	FOREIGN KEY (type) REFERENCES type_complex(ROWID)
-	);'''
-)
-
-cursor.execute('''
-	CREATE TABLE IF NOT EXISTS type_complex (
-	ROWID INTEGER NOT NULL,
-	name TEXT NOT NULL,
-	PRIMARY KEY(ROWID)
-	);'''
-)
-
-cursor.execute('''
-	CREATE TABLE IF NOT EXISTS rounds (
-	ROWID INTEGER NOT NULL,
-	skill_id INT NOT NULL,
-	wod_id INT NOT NULL,
-	PRIMARY KEY(ROWID),
-	FOREIGN KEY (skill_id) REFERENCES skills(ROWID),
-	FOREIGN KEY (wod_id) REFERENCES wods(ROWID)
-	);'''
-)
-
-cursor.execute('''
-	CREATE TABLE IF NOT EXISTS exercise (
-	ROWID INTEGER NOT NULL,
-	round_id INT NOT NULL,
-	repeats INT NOT NULL,
-	weight INT NOT NULL,
-	name TEXT NOT NULL,
-	PRIMARY KEY(ROWID),
-	FOREIGN KEY (round_id) REFERENCES rounds(ROWID)
-	);'''
-)
-
-# date = input("Дата: ")
-skill_type = input("Тип навыка ")
-skill_timecap = input("Время навыка ")
-
-cursor.execute('''
-	INSERT INTO skills ('timecap', type)
+	INSERT INTO wods ('timecap', 'type')
 	SELECT ? as timecap, ROWID FROM type_complex 
 	WHERE name = ?
-''', (skill_timecap, skill_type))
+''', (wod_timecap, wod_type))
+
+
+[skill_id], = cursor.execute("SELECT ROWID FROM skills ORDER BY ROWID DESC LIMIT 1")
+[wod_id], = cursor.execute("SELECT ROWID FROM wods ORDER BY ROWID DESC LIMIT 1")
+
+print(type(skill_id))
+cursor.execute('''
+	INSERT INTO days ('date', 'skill_id', 'wod_id')
+	VALUES (?, ?, ?)
+''', (date, skill_id, wod_id))
+
+cursor.execute('''
+	INSERT INTO rounds ('skill_id')
+	VALUES (?)
+	''', (skill_id,))
+
+cursor.execute('''
+	INSERT INTO rounds ('wod_id')
+	VALUES (?)
+	''', (wod_id,))
 
 conn.commit()
 
---
+++ db.py
@@ -0,0 +1,74 @@
+
+def create_db(conn):
+	with conn:
+		cursor = conn.cursor()
+		cursor.execute('''
+			CREATE TABLE IF NOT EXISTS days (
+			ROWID INTEGER NOT NULL,
+			date TEXT NOT NULL,
+			skill_id INT NOT NULL,
+			wod_id INT NOT NULL,
+			PRIMARY KEY(ROWID),
+			FOREIGN KEY (skill_id) REFERENCES skills(ROWID),
+			FOREIGN KEY (wod_id) REFERENCES wods(ROWID)
+			);'''
+		)
+
+		cursor.execute('''CREATE TABLE IF NOT EXISTS skills (
+			ROWID INTEGER NOT NULL,
+			type TEXT NOT NULL,
+			timecap TEXT NOT NULL,
+			PRIMARY KEY(ROWID),
+			FOREIGN KEY (type) REFERENCES type_complex(ROWID)
+			);'''
+		)
+
+		cursor.execute('''
+			CREATE TABLE IF NOT EXISTS wods (
+			ROWID INTEGER NOT NULL,
+			type TEXT NOT NULL,
+			timecap TEXT NOT NULL,
+			PRIMARY KEY(ROWID),
+			FOREIGN KEY (type) REFERENCES type_complex(ROWID)
+			);'''
+		)
+
+		cursor.execute('''
+			CREATE TABLE IF NOT EXISTS type_complex (
+			ROWID INTEGER NOT NULL,
+			name TEXT NOT NULL,
+			PRIMARY KEY(ROWID)
+			);'''
+		)
+
+		cursor.execute('''
+			CREATE TABLE IF NOT EXISTS rounds (
+			ROWID INTEGER NOT NULL,
+			skill_id INT,
+			wod_id INT,
+			PRIMARY KEY(ROWID),
+			FOREIGN KEY (skill_id) REFERENCES skills(ROWID),
+			FOREIGN KEY (wod_id) REFERENCES wods(ROWID)
+			);'''
+		)
+
+		cursor.execute('''
+			CREATE TABLE IF NOT EXISTS exercise (
+			ROWID INTEGER NOT NULL,
+			round_id INT NOT NULL,
+			repeats INT NOT NULL,
+			weight INT NOT NULL,
+			name TEXT NOT NULL,
+			PRIMARY KEY(ROWID),
+			FOREIGN KEY (round_id) REFERENCES rounds(ROWID)
+			);'''
+		)
+
+def input_skills(conn, skill_timecap, skill_tipe):
+	with conn:
+		cursor = conn.cursor()
+		cursor.execute('''
+			INSERT INTO skills ('timecap', 'type')
+			SELECT ? as timecap, ROWID FROM type_complex 
+			WHERE name = ?
+		''', (skill_timecap, skill_type))
